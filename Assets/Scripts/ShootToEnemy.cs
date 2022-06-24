b21ef803a575b1efc8094c9e0ac2cabc2efd2828
dnorambu <diego.norambuena@sansano.usm.cl>
2022-06-24 12:27:01 -0400

    First commit

---
+++ Assets/Scripts/ShootToEnemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootToEnemy : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InvokeRepeating();
+        if(bullet == null)
+        {
+            Debug.LogError("There is no bullte prefab");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Shooting
+    private void Shoot()
+    {
+        GameObject bulletCopy = Instantiate(bullet, transform.position, Quaternion.identity);
+
+    } 
+}
